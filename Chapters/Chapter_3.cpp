6249cd0eaffe6bb27a7d3e342bd84daa3c8f6449
Martin Eigenbrodt <pydvlpr@gmail.com>
2022-10-23 12:27:35 +0200

    Moved function add outside main to make chapter_3.cpp comp ileable. Added decriptive output before inputs.

--- Chapters/Chapter_3.cpp
+++ Chapters/Chapter_3.cpp
@@ -20,217 +20,218 @@ notorious errors.
 
 /* Don't worry about these functions, they will be useful
 later on. */
-void func(int i) {
-	i = 5;
-}
+void func(int i) { i = 5; }
 
-void func2(int &i) {
-	i = 5;
+void func2(int &i) { i = 5; }
+
+void add(int &a, int &b) {
+  // Add a and b, and store the result in a
+  std::cout << "adding a and b" << std::endl;
 }
 
 int main() {
-	/*
-	User input
-	==========
-	So far, we've went over how to show the user some text through
-	the console. But how do we get input from the user?
+  /*
+  User input
+  ==========
+  So far, we've went over how to show the user some text through
+  the console. But how do we get input from the user?
 
-	To get input from the user, we will use std::cin, which means
-	Character IN. It works similar to std::cout, but the angle
-	brackets are the other way. 
+  To get input from the user, we will use std::cin, which means
+  Character IN. It works similar to std::cout, but the angle
+  brackets are the other way.
 
-	For example, getting a single number as input could be done like
-	this:
-	*/
+  For example, getting a single number as input could be done like
+  this:
+  */
+  std::cout << "Enter an integer value" << std::endl;
+  int n;
+  std::cin >> n;
 
-	int n;
-	std::cin >> n;
+  /*
+  Note that we don't need to give a value to n as it will get
+  overwritten with the user's input. This is known as
+  declaring a variable without initializing it.
 
-	/*
-	Note that we don't need to give a value to n as it will get
-	overwritten with the user's input. This is known as 
-	declaring a variable without initializing it.
+  However, if you aren't going to give a value to it but rather
+  you're going to be doing something such as incrementing it,
+  you should give it a value as uninitialized variables are
+  assigned a garbage value (a value that isn't useful).
 
-	However, if you aren't going to give a value to it but rather
-	you're going to be doing something such as incrementing it,
-	you should give it a value as uninitialized variables are
-	assigned a garbage value (a value that isn't useful).
+  std::cin works with other types of variables as well!
+  */
 
-	std::cin works with other types of variables as well!
-	*/
+  std::cout << "Enter an string value" << std::endl;
+  std::string s;
+  std::cin >> s;
 
-	std::string s;
-	std::cin >> s;
+  /*
+  A main issue with std::cin is that it ends input at spaces
+  and at newlines. This means that you won't be able to get
+  input such as a sentence using just std::cin. If you want
+  to get an entire line (i.e. including spaces), you must
+  use the getline() function.
+  */
 
-	/*
-	A main issue with std::cin is that it ends input at spaces
-	and at newlines. This means that you won't be able to get
-	input such as a sentence using just std::cin. If you want
-	to get an entire line (i.e. including spaces), you must
-	use the getline() function. 
-	*/
+  std::cout << "Enter a string value to be caputered by getline" << std::endl;
+  getline(std::cin, s);
 
-	getline(std::cin, s);
+  /*
+  This puts the entire line in s.
 
-	/*
-	This puts the entire line in s.
+  What if we want to get multiple inputs from the user, but
+  we don't know how many there are? We can use a while loop
+  to get input until the user enters a certain value.
+  */
 
-	What if we want to get multiple inputs from the user, but
-	we don't know how many there are? We can use a while loop
-	to get input until the user enters a certain value.
-	*/
+  while (true) {
+    std::cout << "Enter an interger value. Value 0 terminates this loop"
+              << std::endl;
+    std::cin >> n;
+    if (n == 0) {
+      break;
+    }
+  }
 
-	while (true) {
-		std::cin >> n;
-		if (n == 0) {
-			break;
-		}
-	}
+  /*
+  Note that we can use the break keyword to exit a loop
+  early. This is useful if you want to exit a loop when
+  a certain condition is met.
 
-	/*
-	Note that we can use the break keyword to exit a loop
-	early. This is useful if you want to exit a loop when
-	a certain condition is met.
+  When the user enters 0, the loop will exit. This is
+  known as a sentinel value, which is a value that
+  indicates that the user is done entering input.
+  */
 
-	When the user enters 0, the loop will exit. This is
-	known as a sentinel value, which is a value that
-	indicates that the user is done entering input.
-	*/
+  /*
+  Pointers
+  ========
+  Pointers *point* to an object. They are declared using the
+  * symbol. For example:
+  */
 
-	/*
-	Pointers
-	========
-	Pointers *point* to an object. They are declared using the
-	* symbol. For example:
-	*/
-	
-	int *p = &n;
+  int *p = &n;
 
-	/*
-	I know, it looks weird. But it's not too hard to understand.
-	*p is the value of the pointer p. &n is the address of n.
-	The & symbol is known as the address-of operator, or the
-	reference operator. It returns the address of the variable
-	that it is used on.
+  /*
+  I know, it looks weird. But it's not too hard to understand.
+  *p is the value of the pointer p. &n is the address of n.
+  The & symbol is known as the address-of operator, or the
+  reference operator. It returns the address of the variable
+  that it is used on.
 
-	To get the value of the pointer, we use the * symbol again.
-	*p returns the value of the pointer p. This is known as
-	dereferencing a pointer.
+  To get the value of the pointer, we use the * symbol again.
+  *p returns the value of the pointer p. This is known as
+  dereferencing a pointer.
 
-	So, if we try to get the value of p and not *p, we will get
-	the address in memory of n. If we try to get the value of *p,
-	we will get the value of n.
+  So, if we try to get the value of p and not *p, we will get
+  the address in memory of n. If we try to get the value of *p,
+  we will get the value of n.
 
-	Note that p is a pointer to an integer, so we can't use it
-	to point to a string. Pointers have types too!
+  Note that p is a pointer to an integer, so we can't use it
+  to point to a string. Pointers have types too!
 
-	In fact, a funny thing about pointers is that you can get
-	the address of a pointer. This is known as a pointer to a
-	pointer. This is useful if you want to pass a pointer to a
-	function, but you don't want to modify the original pointer.
+  In fact, a funny thing about pointers is that you can get
+  the address of a pointer. This is known as a pointer to a
+  pointer. This is useful if you want to pass a pointer to a
+  function, but you don't want to modify the original pointer.
 
-	To get the address of a pointer, we use the & symbol again.
-	*/
+  To get the address of a pointer, we use the & symbol again.
+  */
 
-	int **pp = &p;
+  int **pp = &p;
 
-	/*
-	Notice that we use two * symbols. This is because we are
-	getting the address of a pointer, which is a pointer itself.
-	
-	This is also known as a double pointer. We can get the value
-	of a double pointer by using the * symbol twice.
-	*/
+  /*
+  Notice that we use two * symbols. This is because we are
+  getting the address of a pointer, which is a pointer itself.
 
-	std::cout << **pp << std::endl;
+  This is also known as a double pointer. We can get the value
+  of a double pointer by using the * symbol twice.
+  */
 
-	/*
-	References
-	==========
-	Now, you might have noticed that I used the & symbol to get
-	the address of a pointer. This is because references are
-	just pointers that can't be reassigned. They are declared
-	using the & symbol. For example:
-	*/
+  std::cout << **pp << std::endl;
 
-	int &r = n;
+  /*
+  References
+  ==========
+  Now, you might have noticed that I used the & symbol to get
+  the address of a pointer. This is because references are
+  just pointers that can't be reassigned. They are declared
+  using the & symbol. For example:
+  */
 
-	/*
-	Notice that we don't use the * symbol. This is because
-	references are not pointers. They are just pointers that
-	can't be reassigned.
+  int &r = n;
 
-	We can use references to modify the value of a variable
-	without having to use the * symbol. For example:
-	*/
+  /*
+  Notice that we don't use the * symbol. This is because
+  references are not pointers. They are just pointers that
+  can't be reassigned.
 
-	r = 5;
+  We can use references to modify the value of a variable
+  without having to use the * symbol. For example:
+  */
 
-	/*
-	Generally though, you should use pointers instead of
-	references. References are just pointers that can't be
-	reassigned, so they are less useful than pointers.
-	
-	However, references are useful when you want to pass
-	a variable to a function, and you want to modify the
-	original variable. This is because in C++, when you
-	pass a variable to a function, it is passed by value.
-	This means that the function will get a copy of the
-	variable, and any changes made to the variable in the
-	function will not be reflected in the original variable.
-	*/
+  r = 5;
 
-	int i = 10;
+  /*
+  Generally though, you should use pointers instead of
+  references. References are just pointers that can't be
+  reassigned, so they are less useful than pointers.
 
-	func(i); // Look at the function definition above
+  However, references are useful when you want to pass
+  a variable to a function, and you want to modify the
+  original variable. This is because in C++, when you
+  pass a variable to a function, it is passed by value.
+  This means that the function will get a copy of the
+  variable, and any changes made to the variable in the
+  function will not be reflected in the original variable.
+  */
 
-	std::cout << i << std::endl;
+  int i = 10;
 
-	/*
-	When we run this code, we will get 10. This is because
-	the function func() gets a copy of i, and so when it
-	changes the value of i, it doesn't change the value
-	of the original i, but rather the copy of i.
+  func(i);  // Look at the function definition above
 
-	To fix this, we can do something called pass by reference.
-	*/
+  std::cout << i << std::endl;
 
-	func2(i);
+  /*
+  When we run this code, we will get 10. This is because
+  the function func() gets a copy of i, and so when it
+  changes the value of i, it doesn't change the value
+  of the original i, but rather the copy of i.
 
-	std::cout << i << std::endl;
+  To fix this, we can do something called pass by reference.
+  */
 
-	/*
-	Now, when we run this code, we will get 5. This is because
-	we passed a reference to i to the function func2(). This
-	means that the function will get a reference to i, and so
-	when it changes the value of i, it will change the value
-	of the original i.
+  func2(i);
 
-	Remember the add function that we made in chapter 1? Let's
-	implement it using references.
-	*/
-	
-	void add(int &a, int &b) {
-		// Add a and b, and store the result in a
-	}
-	
-	/*
-	If you did it successfully, congrats! You have finished the lesson
-	for chapter 3, and now you can move onto the tasks.
+  std::cout << i << std::endl;
 
-	Task 1: take in user input and print out the memory address that it's
-	stored at.
+  /*
+  Now, when we run this code, we will get 5. This is because
+  we passed a reference to i to the function func2(). This
+  means that the function will get a reference to i, and so
+  when it changes the value of i, it will change the value
+  of the original i.
 
-	Task 2: try taking pointers to different types of variables and see
-	what happens.
+  Remember the add function that we made in chapter 1? Let's
+  implement it using references.
+  */
 
-	Task 3: what happens if you try to modify a pointer to nothing?
-	eg:
-	int *i = 0;
-	*i = 1;
+  /*
+  If you did it successfully, congrats! You have finished the lesson
+  for chapter 3, and now you can move onto the tasks.
 
-	can you guess why?
-	*/
+  Task 1: take in user input and print out the memory address that it's
+  stored at.
 
-	return 0;
+  Task 2: try taking pointers to different types of variables and see
+  what happens.
+
+  Task 3: what happens if you try to modify a pointer to nothing?
+  eg:
+  int *i = 0;
+  *i = 1;
+
+  can you guess why?
+  */
+
+  return 0;
 }
